b1c782c938274c5b2d7bf94da5de1f4835f9e887
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 14:57:48 +0000

    create BeatBox class, a data class to manupulate assets

---
+++ app/src/main/java/com/landtanin/beatbox/BeatBox.java
@@ -0,0 +1,12 @@
+package com.landtanin.beatbox;
+
+/**
+ * Created by Tanin on 14/11/2017.
+ */
+
+public class BeatBox {
+
+    private static final String TAG = "BeatBox";
+    private static final String SOUNDS_FOLDER = "sample_sounds";
+
+}
