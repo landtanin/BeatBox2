74b6ff3006a8c059ad4a6ba91b05cfb6bd725b93
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 14:59:59 +0000

    init AssetManger var for asset managing, create constructor to take one context for AssetManager accessing

--- app/src/main/java/com/landtanin/beatbox/BeatBox.java
+++ app/src/main/java/com/landtanin/beatbox/BeatBox.java
@@ -1,5 +1,8 @@
 package com.landtanin.beatbox;
 
+import android.content.Context;
+import android.content.res.AssetManager;
+
 /**
  * Created by Tanin on 14/11/2017.
  */
@@ -8,5 +11,11 @@ public class BeatBox {
 
     private static final String TAG = "BeatBox";
     private static final String SOUNDS_FOLDER = "sample_sounds";
+
+    private AssetManager mAssets;
+
+    public BeatBox(Context context) {
+        mAssets = context.getAssets();
+    }
 
 }
