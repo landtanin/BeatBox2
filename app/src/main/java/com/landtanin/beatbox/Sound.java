80d22699e538efeb07853011bb8e80790496d383
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 15:18:01 +0000

    create Sound.java, do some work with sound file name for presenting to users

---
+++ app/src/main/java/com/landtanin/beatbox/Sound.java
@@ -0,0 +1,28 @@
+package com.landtanin.beatbox;
+
+/**
+ * Created by Tanin on 14/11/2017.
+ */
+
+public class Sound {
+
+    private String mAssetPath;
+    private String mName;
+
+    public Sound(String assetPath) {
+
+        mAssetPath = assetPath;
+        String[] components = assetPath.split("/");
+        String filename = components[components.length - 1];
+        mName = filename.replace(".wav", "");
+
+    }
+
+    public String getmAssetPath() {
+        return mAssetPath;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+}
