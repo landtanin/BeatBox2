6fd3b0f9a44467a012d5a282bef3c67ddfc1b401
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 13:51:52 +0000

    wire up SoundAdapter with recyclerView from fragment_beat_box in onCreateView

--- app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
@@ -26,6 +26,7 @@ public class BeatBoxFragment extends Fragment {
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
 
         binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
 
         return binding.getRoot();
     }
