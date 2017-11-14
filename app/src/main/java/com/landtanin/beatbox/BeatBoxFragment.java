ed90a1991837cf261d013e064455c5e9cac0ecce
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 13:37:39 +0000

    setLayoutManager to the binded RecyclerView

--- app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,6 +22,9 @@ public class BeatBoxFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
         return binding.getRoot();
     }
 }
