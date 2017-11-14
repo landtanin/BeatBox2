32aab107fd167e8eda6b18210a2ecd49831b769d
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 13:35:14 +0000

    inflating a binding class (BeatBoxFragment)

--- app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
@@ -1,6 +1,14 @@
 package com.landtanin.beatbox;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.landtanin.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
@@ -8,4 +16,11 @@ public class BeatBoxFragment extends Fragment {
         return new BeatBoxFragment();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
+    }
 }
