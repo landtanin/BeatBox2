6c678322c3fe03cc58086c129d17ea903567acfd
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-14 13:23:03 +0000

    create new blank BeatBoxFragment.java with only one static newInstance() method

---
+++ app/src/main/java/com/landtanin/beatbox/BeatBoxFragment.java
@@ -0,0 +1,11 @@
+package com.landtanin.beatbox;
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+}
