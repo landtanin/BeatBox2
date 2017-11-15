2ec36d2deacfb92f1d92bbb04aeae05c7624d1f7
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-15 13:37:01 +0000

    create mocked of BeatBox in SoundViewModelTest.setUp to use in this unit test

--- app/src/test/java/com/landtanin/beatbox/SoundViewModelTest.java
+++ app/src/test/java/com/landtanin/beatbox/SoundViewModelTest.java
@@ -2,12 +2,18 @@ package com.landtanin.beatbox;
 
 import org.junit.Before;
 
+import static org.mockito.Mockito.mock;
+
 /**
  * Created by Tanin on 15/11/2017.
  */
 public class SoundViewModelTest {
+    private BeatBox mBeatBox;
+
     @Before
     public void setUp() throws Exception {
+
+        mBeatBox = mock(BeatBox.class);
     }
 
 }
