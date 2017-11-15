1f79e0b64ddeda01edf8dec9c9c129ac01db1565
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-15 12:20:44 +0000

    Soundpool will assign ID to each sound in it, add mSoundId in Sound to keep track of that id

--- app/src/main/java/com/landtanin/beatbox/Sound.java
+++ app/src/main/java/com/landtanin/beatbox/Sound.java
@@ -8,6 +8,7 @@ public class Sound {
 
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
 
@@ -24,5 +25,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = mSoundId;
     }
 }
