ea6b2836eb46a229766f51bfaef3d8a11b9dbd40
Tanin Rojanapiansatith <sp_land@hotmail.com>
2017-11-15 13:28:53 +0000

    added test class for SoundViewModel, tick create @before setup method

---
+++ app/src/test/java/com/landtanin/beatbox/SoundViewModelTest.java
@@ -0,0 +1,13 @@
+package com.landtanin.beatbox;
+
+import org.junit.Before;
+
+/**
+ * Created by Tanin on 15/11/2017.
+ */
+public class SoundViewModelTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+}
